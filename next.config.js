3f14e049b79323e18a02b28426850e9e8e32b38a
kaveh <kaveh>
2022-01-25 20:25:09 -0500

    removed rewrites so it can build on netlify

--- next.config.js
+++ next.config.js
@@ -1,32 +1,5 @@
-const { BLOG_URL, POSTS_URL } = process.env
+
 
 module.exports = {
-  async rewrites() {
-    return {
-      beforeFiles: [
-        // These rewrites are checked after headers/redirects
-        // and before all files including _next/public files which
-        // allows overriding page files
-        {
-          source: '/articles',
-          destination: `${BLOG_URL}/articles`,
-        },        
-        {
-          source: '/articles/:path*',
-          destination: `${BLOG_URL}/articles/:path*`,
-        },        {
-          source: '/posts',
-          destination: `${POSTS_URL}/posts`,
-        },        
-        {
-          source: '/posts/:path*',
-          destination: `${POSTS_URL}/posts/:path*`,
-        },
-      ]
-    }
-  },
-  images: {
-    //enter the domain or subdomain where you have WordPress installed 
-    domains: ['businesslinkmedia.com', 'kavehhaddadian.files.wordpress.com', 'localhost:3005', 'wordpress.com'],
-  },
+
 }
