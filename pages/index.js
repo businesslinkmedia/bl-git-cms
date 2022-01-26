20d586eb5de9565a976cf57f66e70e0e4a42fe08
kaveh <kaveh>
2022-01-25 20:31:36 -0500

    re-added /articles to main page - still removed from next.config

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,16 @@ export default function Home() {
               Find in-depth information about Next.js features and API.
             </p>
           </a>
+
+          <a
+            href="/articles"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Articles &rarr;</h3>
+            <p className="mt-4 text-xl">
+              from some other app!
+            </p>
+          </a>
         </div>
         <Carousel className='w-1/2 border-8 rounded-3xl' 
           autoPlay={true} 
