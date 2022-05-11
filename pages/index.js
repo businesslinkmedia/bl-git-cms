804c9f023945c9b4a7658a3d2abb27c6a6f96829
kaveh <kaveh>
2022-05-11 19:49:55 -0400

    removed (most of) frontend

--- pages/index.js
+++ pages/index.js
@@ -11,33 +11,6 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="/">
-            Next.js!
-          </a>
-        </h1>
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="/articles"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Articles &rarr;</h3>
-            <p className="mt-4 text-xl">
-              from some other app!
-            </p>
-          </a>
-        </div>
       </main>
     </div>
   )
